0c6dce83ee759c921304463f89ab7b5eeb042d60
StefFashka <s.muravev@innopolis.university>
2026-06-13 12:15:50 +0300

    Security branch created, added basic session creation logic + security context

--- backend/src/main/java/com/example/SPOT/controller/UserController.java
+++ backend/src/main/java/com/example/SPOT/controller/UserController.java
@@ -5,6 +5,16 @@ import com.example.SPOT.dto.request.UserLoginDTO;
 import com.example.SPOT.dto.request.UserUpdateDTO;
 import com.example.SPOT.dto.response.UserDTO;
 import com.example.SPOT.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import java.util.Collections;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,13 +40,41 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserDTO> signIn(@RequestBody UserLoginDTO loginDTO) {
+    public ResponseEntity<UserDTO> signIn(@RequestBody UserLoginDTO loginDTO, HttpServletRequest request) {
+        UserDTO response = userService.loginUser(loginDTO);
+
+        //old session invalidation
+        HttpSession existingSession = request.getSession(false);
+        if (existingSession != null) {
+            existingSession.invalidate();
+        }
+        
+        //new session creation
+        HttpSession session = request.getSession(true);
+
+        /** set aunthentication params to spring security context
+         * principal - user id
+         * credentials - null (we don't need it after successful login)
+         * authorities - empty list (we don't have roles in our application)
+         **/
+        Authentication authentication = UsernamePasswordAuthenticationToken.authenticated(
+                response.id().toString(),
+                null,
+                Collections.emptyList());
+
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        //set security context to session for spring security
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
+        
         return ResponseEntity.ok()
                 .body(userService.loginUser(loginDTO));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserCreateDTO userCreateDTO) {
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserCreateDTO userCreateDTO, HttpServletRequest request) {
         return ResponseEntity.status(201)
                 .body(userService.createUser(userCreateDTO));
     }
