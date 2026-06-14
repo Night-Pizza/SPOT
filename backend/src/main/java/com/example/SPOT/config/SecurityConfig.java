d10c5a9fe439a8de349062c3e9fd30bea4d964af
Wal1yS <valera.tinyakov@gmail.com>
2026-06-14 10:30:29 +0300

    Update Spring Boot version and add SecurityFilterChain configuration; configure Swagger UI path

--- backend/src/main/java/com/example/SPOT/config/SecurityConfig.java
+++ backend/src/main/java/com/example/SPOT/config/SecurityConfig.java
@@ -25,7 +25,16 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
