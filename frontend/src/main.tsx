87fa65d9c25593cd838429de08a09cbf2e4ca1a8
Katharina Gross <katharina270907@gmail.com>
2026-06-22 15:23:21 +0300

    Updated main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import 'antd/dist/reset.css';
+import 'leaflet/dist/leaflet.css';
 import './index.css';
-import App from './App.tsx';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import { AppProvider } from './contexts/AppContext';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <AuthProvider>
+                <AppProvider>
+                    <ThemeProvider>
+                        <App />
+                    </ThemeProvider>
+                </AppProvider>
+            </AuthProvider>
         </BrowserRouter>
     </StrictMode>,
 );
