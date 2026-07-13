ee41dc851e6e657d142a97f8b14bb0e0291bce6f
StefFashka <s.muravev@innopolis.university>
2026-07-13 18:06:34 +0800

    updates

--- backend/src/main/java/com/example/SPOT/controller/SessionController.java
+++ backend/src/main/java/com/example/SPOT/controller/SessionController.java
@@ -1,5 +1,20 @@
 package com.example.SPOT.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.SPOT.dto.request.SessionCreateDTO;
 import com.example.SPOT.dto.request.SessionUpdateDTO;
 import com.example.SPOT.dto.response.SessionResponseDTO;
@@ -7,13 +22,8 @@ import com.example.SPOT.dto.response.UsersForSessionDTO;
 import com.example.SPOT.model.UserModel;
 import com.example.SPOT.repository.UserRepository;
 import com.example.SPOT.service.SessionService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/session")
